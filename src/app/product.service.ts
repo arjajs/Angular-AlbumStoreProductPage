1eb9667b5447386829c11c18a4f9374c0773013d
Arja Sivapiragasam <arja.sivapiragasam@bouvet.no>
2020-09-01 12:56:13 +0200

    6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http'
 import'rxjs/add/operator/map';
 import { Album } from './album'; 
+import{ Product }from './product'
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl: string = '../assets/album.json';
+  private _productsUrl: string = '../assets/products.json'
 
   constructor(private _http: Http) {  //parameter injtects and instance of a service into the class
 
@@ -15,5 +17,9 @@ export class ProductService {
 
   getAlbum (id : number) : Observable<Album> {
     return this._http.get(this._albumUrl).map(response => <Album>response.json());
+  }
+
+  getProducts (){
+    
   }
 }
